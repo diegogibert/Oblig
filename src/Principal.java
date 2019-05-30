f36a9e3da6091c466c2045c44608a51e9d1c62a3
saguirregaray <saguirregaray@correo.um.edu.uy>
2019-05-30 08:15:49 -0300

    -lo comentado es lo del parse, no anda y no se por que. Lo subo ahi preguntamos en clase

--- src/Principal.java
+++ src/Principal.java
@@ -1,36 +1,75 @@
 import Hash.ElementoYaExistenteException;
 import Hash.HashCerrado;
 import double_linked_list.ValorNoExisteException;
+import uy.edu.um.clases.Athlete;
 import uy.edu.um.clases.NationalOlympicCommittee;
+import uy.edu.um.clases.SexType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
 
 public class Principal {
-    public static void main (String[] args){
+
+    public static void main(String[] args) {
+
+        HashCerrado NationalOlympicCommittees = new HashCerrado(231);
+       // HashCerrado Athletes = new HashCerrado(5000);
+
         BufferedReader objReader = null;
+
         try {
+
             String strCurrentLine;
             objReader = new BufferedReader(new FileReader("noc_regions.csv"));
+
             while ((strCurrentLine = objReader.readLine()) != null) {
                 String[] vec = strCurrentLine.split(",");
-                HashCerrado NationalOlympicCommittees= new HashCerrado(231); //creo que esta mal hacer esto en el while pero si lo pongo afuera me da un error
-                NationalOlympicCommittee temp = new NationalOlympicCommittee(vec[0],vec[1]);
-                NationalOlympicCommittees.insert(temp.getNoc(),temp.getRegion());
-                System.out.println(NationalOlympicCommittees.get(temp.getNoc()));
-
-
+                NationalOlympicCommittee temp = new NationalOlympicCommittee(vec[0], vec[1]);
+                NationalOlympicCommittees.insert(temp.getNoc(), temp.getRegion());
             }
-        } catch (IOException | ElementoYaExistenteException | ValorNoExisteException e) {
+
+
+           // objReader=new BufferedReader(new FileReader("athlete_events.csv"));
+
+//            strCurrentLine=objReader.readLine(); // para que saltee la fila que son los nombres de las columnas
+//            while ((strCurrentLine=objReader.readLine())!=null){
+//                String[] vec = strCurrentLine.split(",");
+//
+//
+//                    Athlete temp= new Athlete(Long.parseLong(vec[0]) , vec[1], SexType.valueOf(vec[2]), Integer.parseInt(vec[3]),
+//                            Float.parseFloat(vec[4]), Float.parseFloat(vec[5]));
+//                    Athletes.insert(temp.getId(),temp);
+//
+//
+//
+//            }
+
+
+
+
+
+
+
+
+
+
+
+        } catch (IOException | ElementoYaExistenteException e ) {
+
             e.printStackTrace();
+
         } finally {
+
             try {
-                if (objReader!= null)     objReader.close();
+
+                if (objReader != null) objReader.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
+
+
 
     }
 }
