0af605ed31b3dd6ff5ae3db168cc4513146a103f
saguirregaray <saguirregaray@correo.um.edu.uy>
2019-06-03 20:05:24 -0300

    -Separe las cosas

--- src/Principal.java
+++ src/Principal.java
@@ -1,26 +1,11 @@
-import BinarySearchTree.BinarySearchTree;
-import BinarySearchTree.ValorYaExisteException;
-import Hash.ElementoYaExistenteException;
-import Hash.HashCerrado;
 import Utilities.LoadData;
 import Utilities.Menu;
-import double_linked_list.ListaVaciaException;
-import double_linked_list.ValorNoExisteException;
-import heap.HeapNode;
-import uy.edu.um.clases.*;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
-import static uy.edu.um.clases.SexType.valueOf;
 
 public class Principal {
 
     public static void main(String[] args) {
-
 
         try {
             LoadData.load();
@@ -29,11 +14,6 @@ public class Principal {
             System.out.println("Ingrese una opcion valida");
             new Menu();
         }
-
-
-
-
-
 
 
     }
